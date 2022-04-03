4e3cc6d8d55c9fa2dffba0d0d4d6b94cdcec8216
obaedulislam <obaed.cse@gmail.com>
2022-04-03 16:51:40 +0600

    Item detail input field done

--- app.js
+++ app.js
@@ -9,3 +9,38 @@ detailSubmitBtn.addEventListener("click", function(){
 
   buyerDetails.value = '';
 });
+
+// Item details information
+const addProductBtn = document.getElementById("add-details-btn");
+addProductBtn.addEventListener("click", function(){
+  const itemName = document.getElementById("item-name-input").value;
+  const itemPrice = document.getElementById("item-price-input").value;
+  const itemQuantity = document.getElementById("item-quantity-input").value;
+
+/* <tr>
+    <th >1</th>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+</tr> */
+
+  const infoTable = document.getElementById("info-table");
+  const tr = document.createElement('tr');
+  const th = document.createElement('th');
+  const td1 = document.createElement('td');
+  const td2 = document.createElement('td');
+  const td3 = document.createElement('td');
+  const totalPrice = parseInt(itemPrice) * parseInt(itemQuantity);
+
+  th.innerText = itemName; // <th >1</th>
+  td1.innerText = itemPrice;//<td>Mark</td>
+  td2.innerText = itemQuantity;
+  td3.innerText = totalPrice;
+
+  tr.appendChild(th);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  infoTable.appendChild(tr);
+  
+});
